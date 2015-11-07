b94b38b7a296f54499b7fe318331d0aed0cb9cee
Rohan Taori <rtaori13@gmail.com>
2015-11-06 21:02:39 -0800

    added organization

--- src/Compressor.java
+++ src/Compressor.java
@@ -1,3 +1,4 @@
+package src;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
--- src/ServerRun.java
+++ src/ServerRun.java
@@ -1,3 +1,4 @@
+package src;
 
 /** CHAT APP - SERVER SIDE IMPLEMENTATION **/
 
